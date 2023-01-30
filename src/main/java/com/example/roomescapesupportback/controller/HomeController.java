3a9681b8b995d559ab66d2f7fae1a046753cc6c6
junchanpp <9pruruluru@naver.com>
2023-01-30 20:31:27 +0900

    api response 변경

--- src/main/java/com/example/roomescapesupportback/controller/HomeController.java
+++ src/main/java/com/example/roomescapesupportback/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.roomescapesupportback.controller;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
 
   @GetMapping("/")
-  public String home() {
-    return "Hello, World!";
+  public ResponseEntity<String> home() {
+    return ResponseEntity.badRequest().build();
   }
 }
