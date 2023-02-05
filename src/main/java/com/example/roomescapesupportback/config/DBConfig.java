7f7e426dc7c3635733b1429bb546350cc0d11c8a
junchanpp <9pruruluru@naver.com>
2023-02-05 21:35:05 +0900

    db 정보 application.properties로 이동 및 실제 db 연동

--- src/main/java/com/example/roomescapesupportback/config/DBConfig.java
+++ src/main/java/com/example/roomescapesupportback/config/DBConfig.java
@@ -2,6 +2,7 @@ package com.example.roomescapesupportback.config;
 
 import com.zaxxer.hikari.HikariDataSource;
 import javax.sql.DataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,14 +10,24 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
 public class DBConfig {
+  @Value("${spring.datasource.url}")
+  private String dbUrl;
+
+  @Value("${spring.datasource.username}")
+  private String username;
+
+  @Value("${spring.datasource.password}")
+  private String password;
+
+  @Value("${spring.datasource.driver-class-name}")
+  private String driverClassName;
 
   @Bean
   public DataSource dataSource() {
-    return DataSourceBuilder.create().url(
-            "jdbc:mysql://localhost:3306/test?serverTimezone=Asia/Seoul&characterEncoding=UTF-8&allowPublicKeyRetrieval=true&useSSL=false")
-        .username("root")
-        .password("test123")
-        .driverClassName("com.mysql.cj.jdbc.Driver")
+    return DataSourceBuilder.create().url(dbUrl)
+        .username(username)
+        .password(password)
+        .driverClassName(driverClassName)
         .type(HikariDataSource.class)
         .build();
   }
